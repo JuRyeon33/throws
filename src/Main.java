29ffacba4ac8bc8fea9c42961ff7dd76f9e99ba8
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-17 13:07:59 +0900

    throws

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int converToInt(String s) throws NumberFormatException {
+        return Integer.parseInt(s);
+    }
+}
