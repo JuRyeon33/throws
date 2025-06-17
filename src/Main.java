b6f6101e7cac35025d1d5bfa5bc2c8c537f51a25
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-17 14:38:55 +0900

    convertToInt test

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        String input = "10";
+        String test = "24";
 
         try {
-            boolean result = validateTokenCount(input);
-
-            if (result) {
-                System.out.println("숫자 2개");
-            }
-        } catch(NumberFormatException e) {
+            int result = multiply(test);
+            System.out.println(result);
+        } catch (NumberFormatException e) {
             System.out.println(e.getMessage());
+
         }
-
-
     }
 
-    public static boolean validateTokenCount(String tokens) throws NumberFormatException {
-        if (tokens.length() != 2) {
-            throw new NumberFormatException("숫자 2개가 아닙니다.");
+    public static int multiply(String input) throws NumberFormatException {
+        if (input.length() != 2) {
+            throw new NumberFormatException("숫자 두개 여야 함");
         }
 
-        return true;
+        int inputA = Integer.parseInt(input.substring(0, 1));
+        int inputB = Integer.parseInt(input.substring(1));
+
+        return inputA * inputB;
     }
 }
