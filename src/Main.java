ce23c11b53f8380ee60df6099d0775e1c5152e98
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-17 13:34:13 +0900

    throws

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@
 
 public class Main {
     public static void main(String[] args) {
+        String strNum = "123a";
 
+        try {
+            int num = convertToInt(strNum);
+
+            System.out.println("변환 결과 : " + num);
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 형식 오류");
+        }
     }
 
-    public static int converToInt(String s) throws NumberFormatException {
+    public static int convertToInt(String s) throws NumberFormatException {
         return Integer.parseInt(s);
     }
 }
